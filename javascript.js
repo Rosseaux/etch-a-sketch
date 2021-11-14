94abd8a43be7e5864e73d6e265f53d0301522c57
Ross Hetherton <rosshetherton@live.com>
2021-11-14 15:41:14 +1100

    Finished application, colour switcher is working perfectly and the styling is finalised. Proud of this one!

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
 const gridContainer = document.querySelector(".gridContainer");
 let size = document.querySelector(".sizeSlider").value;
+let drawColour = document.querySelector("#colour").value;
 
-
-const colourButton = document.querySelector("#colour");
-
+//initialise the grid on page load
 start();
-
 function start() {
     gridCreate(size);
     sliderDisplay();
@@ -21,22 +19,23 @@ function gridCreate(cellCount) {
         gridContainer.style.gridTemplateRows = `repeat(${cellCount}, 1fr)`;
         gridContainer.appendChild(cell);
         cell.addEventListener("mouseover", function(event) {
-            event.target.style.backgroundColor = "black";
-        })
+            event.target.style.backgroundColor = drawColour;
+        });
     }
-
 }
 
+//Colour switcher, proud of this as it doesn't re-set the grid allowing you to
+//use multiple colours on the drawing
+const colourButton = document.querySelector("#colour");
+colourButton.onchange = () => drawColour = document.querySelector("#colour").value;
+
+
+//listener for the resize button 
 const resizeButton = document.querySelector("#resize");
 resizeButton.addEventListener("click", () => {
     const resizeValue = document.querySelector(".sizeSlider").value;
     resize(resizeValue);
 });
-
-
-
-
-
 
 //clears grid, had to get some help from user Silende87's code with this one as I struggled.
 function clearGrid() {
@@ -44,6 +43,7 @@ function clearGrid() {
     gridCreate(size);
 }
 
+//listener for clear button
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
     clearGrid();
@@ -53,13 +53,13 @@ clearButton.addEventListener("click", () => {
 function sliderDisplay() {
     const sizeSlider = document.querySelector(".sizeSlider");
     const sliderDisplay = document.querySelector("#display");
-    sliderDisplay.textContent = `Grid Size: ${sizeSlider.value}`;
+    sliderDisplay.textContent = `Grid Size: ${sizeSlider.value} x ${sizeSlider.value}`;
     sizeSlider.addEventListener("input", function() {
-        sliderDisplay.textContent = `Grid Size: ${sizeSlider.value}`;
+        sliderDisplay.textContent = `Grid Size: ${sizeSlider.value} x ${sizeSlider.value}`;
     });
     
 }
-
+//resize function
 function resize() {
     gridContainer.innerHTML = "";
     size = document.querySelector(".sizeSlider").value;
