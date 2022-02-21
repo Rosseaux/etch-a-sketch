e3cbf8b34a6cd7de79184f06713d555ec5d2d7cb
Ross Hetherton <rosshetherton@192-168-1-117.tpgi.com.au>
2022-02-22 10:48:24 +1100

    selective env file?

--- .github/workflows/readmeio_publish.js
+++ .github/workflows/readmeio_publish.js
@@ -1,11 +1,11 @@
 const sdk = require('api')('@developers/v2.0#5p9er16kx9dx3ib');
 require('dotenv').config();
 
-sdk.auth(`NqT6pqeHehWcFJ4SbPiy5t0LCvcL2eMp`)
+sdk.auth('NqT6pqeHehWcFJ4SbPiy5t0LCvcL2eMp')
 sdk.createChangelog({
   hidden: false,
-  title: `v2.3`,
-  body: `release version 2.3`,
+  title: "2.3",
+  body: "testing script push",
 })
   .then(res => console.log(res))
   .catch(err => console.error(err));
