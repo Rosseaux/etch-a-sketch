7563052215aacd1f3832d10067f4020cfcbf8a1a
Ross Hetherton <rosshetherton@192-168-1-117.tpgi.com.au>
2022-02-22 11:10:55 +1100

    got action working and pushing to api, readding env file

--- readmeio_publish.js
+++ readmeio_publish.js
@@ -1,13 +1,11 @@
 const sdk = require('api')('@developers/v2.0#5p9er16kx9dx3ib');
 require('dotenv').config();
 
-sdk.auth('NqT6pqeHehWcFJ4SbPiy5t0LCvcL2eMp')
+sdk.auth(`${process.env['README_TOKEN']}`)
 sdk.createChangelog({
   hidden: false,
-  title: "2.3",
-  body: "testing script push",
+  title: `${process.env['RELEASE_NAME']} :${process.env['RELEASE_TITLE']}`,
+  body: `${process.env['RELEASE_BODY']}`,
 })
   .then(res => console.log(res))
   .catch(err => console.error(err));
-
-  //something to change file 
